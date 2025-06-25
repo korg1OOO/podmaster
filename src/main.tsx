66f4d15c2fbe02f148c932c82384be7cf44dae48
korg1OOO <enzomichelfeldman@gmail.com>
2025-06-25 09:05:40 -0300

    Adding Utmify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { useEffect } from "react";
+
+function Root() {
+  useEffect(() => {
+    window.pixelId = "685be3be0a03918eb3fab5a0";
+    const script = document.createElement("script");
+    script.async = true;
+    script.defer = true;
+    script.src = "https://cdn.utmify.com.br/scripts/pixel/pixel.js";
+    document.head.appendChild(script);
+  }, []);
+
+  return <App />;
+}
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Failed to find root element");
 }
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(<Root />);
